557ef44fdab73ea06fd0b4c08eaee7701fa2fea7
Zin PHtet <zinpainghtet.215108@gmail.com>
2022-02-06 15:23:11 +0630

    accum

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 //CodeWars Challenge
+// 1 highAndLow
 const highAndLow = (numbers) => {
 	const numArr = numbers.split(' ').map((num) => Number(num));
 	return `${Math.max(...numArr)} ${Math.min(...numArr)}`;
 };
 // highAndLow('1 2 -3 4 5');
 
-//Sort words according to their orders
+// 2 Sort words according to their orders
 const order = (words) => {
 	if (!words.length) return '';
 	const splitWords = words.split(' ');
@@ -27,6 +28,16 @@ const order = (words) => {
 	return `${sortedArr.join(' ')}`;
 };
 
-order('is2 Thi1s T4est 3a');
-order('4of Fo1r pe6ople g3ood th5e the2');
-order('');
+// order('is2 Thi1s T4est 3a');
+// order('4of Fo1r pe6ople g3ood th5e the2');
+// order('');
+
+//3 Acc
+const accum = (string) => {
+	return string
+		.split('')
+		.map((char, idx) => char.toLowerCase().repeat(idx + 1))
+		.map((word) => word[0].toUpperCase() + word.slice(1))
+		.join('-');
+};
+accum('abcd');
