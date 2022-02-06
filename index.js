7708d51a96d1bcda0c9ed53556fa5ef59df9d99f
Zin PHtet <zinpainghtet.215108@gmail.com>
2022-02-06 13:47:09 +0630

    order words

--- index.js
+++ index.js
@@ -4,3 +4,29 @@ const highAndLow = (numbers) => {
 	return `${Math.max(...numArr)} ${Math.min(...numArr)}`;
 };
 // highAndLow('1 2 -3 4 5');
+
+//Sort words according to their orders
+const order = (words) => {
+	if (!words.length) return '';
+	const splitWords = words.split(' ');
+
+	const filterNumChar = splitWords.map((words) =>
+		words.split('').filter((char) => Number(char))
+	);
+
+	const numSort = filterNumChar
+		.flat()
+		.map((num) => Number(num))
+		.sort();
+
+	const sortedArr = [];
+	numSort.forEach((num) => {
+		const myWord = splitWords.filter((word) => word.includes(num.toString()));
+		sortedArr.push(myWord);
+	});
+	return `${sortedArr.join(' ')}`;
+};
+
+order('is2 Thi1s T4est 3a');
+order('4of Fo1r pe6ople g3ood th5e the2');
+order('');
